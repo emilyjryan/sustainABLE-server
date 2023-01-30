275de154d6f4364065a14e03952a937e24fa5636
emilyjryan <emilyryanmac@gmail.com>
2023-01-30 13:06:35 -0800

    now adding favs to favHabits in User model

--- controllers/api-v1/favorites.js
+++ controllers/api-v1/favorites.js
@@ -2,9 +2,31 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../../models')
+const authLockedRoute = require('./authLockedRoute')
 
 // ROUTES
 
 // POST /favorites -- add a favorite to the users favHabits array
+router.post('/', authLockedRoute, async (req, res) => {
+    try {
+        const user = res.locals.user
+        if (!req.body.id) {
+            res.status(404).json({ msg: 'Habit not found' })
+            return
+        }
+        user.favHabits.push(req.body.id)
+        await user.save()
+        res.sendStatus(201)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: 'Server Error' })
+    }
+})
+
+//GET route
+router.get('/', authLockedRoute, async (req, res) => {
+    console.log(res.locals.user)
+    res.json({favHabits: res.locals.user.favHabits})
+})
 
 module.exports = router
