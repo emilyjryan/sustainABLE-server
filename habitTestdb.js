bfed00b7409e10fe7e0e1a1a3880354ff93b1df5
curtlen88 <curtlen88@gmail.com>
2023-02-01 15:14:28 -0700

    preps for deployment

--- habitTestdb.js
+++ habitTestdb.js
@@ -34,3 +34,44 @@ const habitCrud = async () => {
     }
 }
 // habitCrud()
+
+export default function HabitsForm(props) {
+    
+    return (
+        <div className="form-floating mx-auto w-75 mb-3 mt-4">
+            {/* form to create a new habit */}
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <label htmlFor="habit">Habit name:</label>
+                    <input  
+                        className="form-control text-center mb-3"
+                        type='text'
+                        id='habit'
+                        placeholder="name your habit here"
+                        value={props.form.habit}
+                        onChange={e => props.setForm({ ...props.form, habit: e.target.value })}
+                        />
+                    <label htmlFor="description">Description</label>
+                    <input  
+                        className="form-control text-center mb-3"
+                        type='text'
+                        id='description'
+                        placeholder="describe your habit here"
+                        value={props.form.description}
+                        onChange={e => props.setForm({ ...props.form, description: e.target.value })}
+                        />
+                    <label htmlFor="imgURL">Picture URL:</label>
+                    <input  
+                        className="form-control text-center"
+                        type='text'
+                        id='imgURL'
+                        placeholder="add an image URL"
+                        value={props.form.imgURL}
+                        onChange={e => props.setForm({ ...props.form, imgURL: e.target.value })}
+                        />
+                </div>
+                <button className="btn btn-primary mt-3" type='submit'>Submit</button>
+            </form>
+        </div>
+    )
+}
